51de88ae0fb0a055417c1d34acade35dc687147b
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-06-05 18:07:56 +0100

    implemented card gen page and adjusted other pages

--- script/documentations.js
+++ script/documentations.js
@@ -3,17 +3,18 @@ const cardDesign = document.querySelector('.card-design')
 const leftColor = document.querySelector('.left-color')
 const midColor = document.querySelector('.mid-color')
 const rightColor = document.querySelector('.right-color')
+const lastColor = document.querySelector('.last-color')
 
 leftColor.addEventListener('click', ()=>{
 	cardDesign.style.background = 'linear-gradient(134deg, #EB001B -43.28%, #F79E1B  97%)'
-	console.log(cardDesign)
 })
 midColor.addEventListener('click', ()=>{
 	cardDesign.style.background = 'linear-gradient(134deg, #327A60 35.63%, #230B58 93.35%)'
-	console.log(cardDesign)
 })
 rightColor.addEventListener('click', ()=>{
 	cardDesign.style.background = 'linear-gradient(134deg, #4C0964 7.86%, #EF8B16 97%)'
-	console.log(cardDesign)
+})
+lastColor.addEventListener('click', ()=>{
+	cardDesign.style.background = 'linear-gradient(40deg, #EF8B16 8.63%, #26619F 98%'
 })
 // documentations page script end
