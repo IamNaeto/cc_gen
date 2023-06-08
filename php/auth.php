a68b630f25c9452b40a0a808b25612a01d3e4563
Nginika <nginikaugochukwu@gmail.com>
2023-06-08 06:25:46 +0000

    nothing has changed

--- php/auth.php
+++ php/auth.php
@@ -4,6 +4,6 @@ session_start();
 if($_SESSION["username"]) {
 	header("Location:../html/card-gen.html");
 } else {
-	header("Location:../html/sign-up.html");
+	header("Location:../html/sign-in.html");
 }
 ?>
